8bb0958eed6512139a09b3be80282160246eb878
Tu Dinh <dinhngoc.tu@irit.fr>
2023-06-02 11:36:59 +0200

    autoshare: always use local file

--- src/view/sharing/fileAgeQuery.ts
+++ src/view/sharing/fileAgeQuery.ts
@@ -1,13 +1,8 @@
 import { IFileAgeQuery } from "./iFileAgeQuery";
-import * as vscode from 'vscode';
 
 export class FileAgeQuery implements IFileAgeQuery {
-    async askOverride(filename: string): Promise<boolean> {
-        let choice = await vscode.window.showInformationMessage("The file "+filename+" is newer on the filesystem than on collaboration, what do you wish to do?", "Use Local","Use Collaboration","Don't autoshare");
-        if(choice === "Don't autoshare") {
-            throw new Error("Cannot autoshare file "+filename+" user has declined to autoshare");
-        }
-        return choice === "Use Local";
+    askOverride(_filename: string): Promise<boolean> {
+        return Promise.resolve(true);
     }
 
 }
