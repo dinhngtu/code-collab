4e8ccd87576a16e4adae0df52d842fa6c9603164
Tu Dinh <dinhngoc.tu@irit.fr>
2023-06-02 11:31:08 +0200

    neutralize irrelevant commands

--- src/extension.ts
+++ src/extension.ts
@@ -2,16 +2,12 @@
 
 import * as vscode from 'vscode';
 import { CollaborationTreeDataProvider } from './view/tree/collaborationTreeDataProvider';
-import { TeletypeConnector } from './view/connector/teletypeConnector';
-import { YJSConnector } from './view/connector/yjsConnector';
 import { MultiConnector } from './view/connector/multiConnector';
 import { PeerFileDecorationProvider } from './view/tree/peerFileDecorationProvider';
 import { ExtensionContext } from './extensionContext';
 import { IEditorSync } from './sync/iEditorSync';
 import { SharingFileDecorationProvider } from './view/sharing/sharingFileDecorationProvider';
-import { FileSharer } from './view/sharing/fileSharer';
 import { SyncConnection } from './binding/syncConnection';
-import { ConnectionTreeElement } from './view/tree/connectionTreeElement';
 import envPaths from "env-paths";
 import { CodeServerConnector } from './view/connector/codeServerConnector';
 import { FrontendConnectionViewProvider } from './view/frontendConnection/frontendConnectionViewProvider';
@@ -20,16 +16,15 @@ import { UserStorage } from './storage/userStorage';
 
 let extensionContext = ExtensionContext.default();
 let shareProvider = new SharingFileDecorationProvider(extensionContext.connectionManager);
-let fileSharer = new FileSharer(extensionContext.connectionManager, shareProvider);
 
 export function activate(context: vscode.ExtensionContext) {
 
 	extensionContext.extensionKind = context.extension.extensionKind;
 	determineCodeServer(context);
-	
+
 	let extensionMode = context.extension.extensionKind === vscode.ExtensionKind.UI ? "UI" : "Backend";
 	console.log("Starting the collaboration extension in mode "+extensionMode+ " CodeServer="+extensionContext.isCodeServer);
-	
+
 	let webViewProvider = new FrontendConnectionViewProvider(extensionContext);
 	context.subscriptions.push(
 		vscode.window.registerWebviewViewProvider("extension.collabConnection", webViewProvider));
@@ -48,14 +43,12 @@ async function setupExtension(webviewProvider : FrontendConnectionViewProvider, 
 	vscode.window.registerFileDecorationProvider(shareProvider);
 
 	let userStorage = new UserStorage(workspaceState, extensionContext);
-	let teletypeConnector = new TeletypeConnector(userStorage, extensionContext);
-	let yjsConnector = new YJSConnector(userStorage, extensionContext);
 	let codeServerConnector = new CodeServerConnector(userStorage,extensionContext);
-	let multiConnector = new MultiConnector(extensionContext, [yjsConnector, teletypeConnector,codeServerConnector]);
+	let multiConnector = new MultiConnector(extensionContext, [codeServerConnector]);
 
 	vscode.window.registerTreeDataProvider("extension.collaboration", new CollaborationTreeDataProvider(extensionContext.connectionManager));
 
-	createCommands(multiConnector);
+	createCommands();
 
 	multiConnector.restoreConnections();
 
@@ -73,44 +66,12 @@ function determineCodeServer(context: vscode.ExtensionContext) {
 	}
 }
 
-function createCommands(multiConnector: MultiConnector) {
-	vscode.commands.registerCommand("extension.addCollabConnection", async () => {
-		try {
-			await multiConnector.newConnection();
-		} catch (error) {
-			await vscode.window.showErrorMessage(error.message);
-		}
-	});
-
-	vscode.commands.registerCommand("extension.removeCollabConnection", async (element: ConnectionTreeElement) => {
-		try {
-			extensionContext.connectionManager.remove(element.connection);
-		} catch (error) {
-			await vscode.window.showErrorMessage(error.message);
-		}
-	});
-
+function createCommands() {
 	vscode.commands.registerCommand("extension.openCollabFile", (syncConnection: SyncConnection, editorSync: IEditorSync) => {
 		syncConnection.shareRemoteToLocal.activateRemoteFile(editorSync);
 	});
-
-	vscode.commands.registerCommand("extension.shareItem", async (uri: vscode.Uri) => {
-		try {
-			await fileSharer.shareFile(uri);
-		} catch (error) {
-			await vscode.window.showErrorMessage(error.message);
-		}
-	});
-
-	vscode.commands.registerCommand("extension.unshareItem", async (uri: vscode.Uri) => {
-		try {
-			await fileSharer.unshareFile(uri);
-		} catch (error) {
-			await vscode.window.showErrorMessage(error.message);
-		}
-	});
 }
 
-export function deactivate() { 
+export function deactivate() {
 	extensionContext.connectionManager.dispose();
 }
